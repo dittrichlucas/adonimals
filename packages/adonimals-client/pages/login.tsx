9f261a45a6e17249e4c37f9e2e9315604e4227ba
dittrichlucas <ld.dittrich@gmail.com>
2022-01-15 16:38:52 -0300

    refactor: small refactorings
    Signed-off-by: dittrichlucas <ld.dittrich@gmail.com>

--- packages/adonimals-client/pages/login.tsx
+++ packages/adonimals-client/pages/login.tsx
@@ -1,7 +1,10 @@
 import { Box, Button, TextField, Title } from 'adonimals-ui'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 const Login: NextPage = () => {
+    const router = useRouter()
+    
     return (
         <Box
             display='flex'
@@ -12,7 +15,7 @@ const Login: NextPage = () => {
             justifyContent='center'
             backgroundColor='greyscale.100'
         >
-            <Title variant='heading4'>Login</Title>
+            <Title variant='heading4' marginTop='0'>Login</Title>
             <TextField 
                 variant='outlined'
                 color='default'
@@ -33,9 +36,11 @@ const Login: NextPage = () => {
                 variant='contained'
                 size='small'
                 width='140px'
+                marginTop='2em'
                 disabled={ false }
                 color='primary'
                 backgroundColor='primary'
+                onClick={ () => router.push('/') }
             >Login
             </Button>
         </Box>
